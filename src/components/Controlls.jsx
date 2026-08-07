30fe2f492f1c509e35ba25a8d9d46b203130f5eb
francoflores <franconahuelf23@gmail.com>
2026-08-06 22:00:13 -0300

    feat: estilizar título, textarea y controles de checkboxes

--- src/components/Controlls.jsx
+++ src/components/Controlls.jsx
@@ -6,31 +6,36 @@ const Controlls = ({
   limitValue,
   handleLimitValue }) => {
   return (
-    <div>
-      <label>
-        <input
-          type="checkbox"
-          checked={excludeSpaces}
-          onChange={() => handleExcludeSpaces(!excludeSpaces)}
-        />
-        Excluir espacios
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          checked={limitCharacter}
-          onChange={handleChangeInputLimit}
-        />
-        Limite de caracteres
-      </label>
-      {
-        limitCharacter &&
-        <input
-          type="number"
-          value={limitValue}
-          onChange={(e) => handleLimitValue(e.target.value)}
-        />
-      }
+    <div className="controles-texto">
+      <div className="grupo-checkboxes">
+        <label>
+          <input
+            type="checkbox"
+            checked={excludeSpaces}
+            onChange={() => handleExcludeSpaces(!excludeSpaces)}
+          />
+          Excluir espacios
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={limitCharacter}
+            onChange={handleChangeInputLimit}
+          />
+          Limite de caracteres
+        </label>
+        {
+          limitCharacter &&
+          <input
+            type="number"
+            value={limitValue}
+            onChange={(e) => handleLimitValue(e.target.value)}
+          />
+        }
+      </div>
+      <div>
+        <p>Approx. reading time: &lt;1 minute</p>
+      </div>
     </div>
   )
 }
