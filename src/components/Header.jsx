29c3cdfc3f5ab0e504e5de4b7acc3cfd5d760909
francoflores <franconahuelf23@gmail.com>
2026-08-06 21:25:10 -0300

    feat: agregar logo al header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react"
+import logo from "../assets/images/logo.png"
 
 const Header = ({ dark, handleDarkTheme }) => {
-  return <header>
-    <div>
-      <h1>Character Counter</h1>
-    </div>
-    <button onClick={() => handleDarkTheme(!dark)}>☀</button>
-  </header>
+  return (
+    <header>
+      <div className="title-container">
+        <img src={logo} alt="Logo de Character Counter" className="logo" />
+        <h1>Character Counter</h1>
+      </div>
+      <button onClick={() => handleDarkTheme(!dark)}>☀</button>
+    </header>
+  )
 }
 
 export { Header }
